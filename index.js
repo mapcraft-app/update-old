d64a65af954309936f29eeee360bc2e4fc18cfe2
Clement bertrand <clement.bertrand22@gmail.com>
2021-10-20 16:59:49 +0200

    Modify update system for new system

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const process = require('process');
 const child = require('child_process');
-const path = require('path');
 const fs = require('fs');
-const OS = require('os');
 const AdmZip = require('adm-zip');
-const DIRNAME = process.cwd();
 
-let Resource = new AdmZip(path.join(DIRNAME, 'temp', 'update_archive.zip'));
-Resource.extractAllTo(DIRNAME, true);
-fs.rm(path.join(DIRNAME, 'temp'), { recursive: true, force: true });
+let Resource = new AdmZip(process.env.ZIP_PATH);
+Resource.extractAllTo(process.env.UNZIP_PATH, true);
+fs.rmSync(process.env.ZIP_PATH, { recursive: true, force: true });
 
 //#region Restart application
-var MAPCRAFT_EXEC;
-if (OS.platform() === 'win32') MAPCRAFT_EXEC = 'mapcraft.exe';
-else MAPCRAFT_EXEC = 'mapcraft';
-const EXECUTABLE = path.join(process.cwd(), MAPCRAFT_EXEC);
-var relaunch = child.spawn(EXECUTABLE, {detached: true, stdio: ['ignore' /* stdin */, 'ignore' /* stdout */, 'ignore' /* stderr */]});
+var relaunch = child.spawn(process.env.EXECUTE_PATH, {detached: true, stdio: ['ignore', 'ignore', 'ignore']});
 relaunch.unref();
 process.exit(0);
 //#endregion
